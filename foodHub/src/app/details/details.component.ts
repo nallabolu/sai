5b2397cdba412112f4e834ae5e21a8741edad485
nallabolu <kalyan.ns@gmail.com>
2018-08-21 16:04:25 +0530

    Signed-off-by: nallabolu <kalyan.ns@gmail.com>

--- foodHub/src/app/details/details.component.ts
+++ foodHub/src/app/details/details.component.ts
@@ -10,10 +10,10 @@ export class DetailsComponent implements OnInit {
 
   trendingList: Resturant[];
   constructor() {
-    const karbakery = { id: 101, name: 'Karachi Bakery', status: 'open', rating: 4.5, image: 'assests/images/kara.png' };
-    const Paradisebakery = { id: 102, name: 'Paradise Bakery', status: 'open', rating: 5.0, image: 'assests/images/para.jpg' };
-    const shahgousebakery = { id: 103, name: 'Shah Gouse Bakery', status: 'close', rating: 3.5, image: 'assests/images/shah.jpg' };
-    const Luckybakery = { id: 104, name: 'Lucky Bakery', status: 'open', rating: 3.0, image: 'assests/images/luc.jpg' };
+    const karbakery = { id: 101, name: 'Karachi Bakery', status: 'open', rating: 4.5, image: 'assets/images/kara.png' };
+    const Paradisebakery = { id: 102, name: 'Paradise Bakery', status: 'open', rating: 5.0, image: 'assets/images/para.jpg' };
+    const shahgousebakery = { id: 103, name: 'Shah Gouse Bakery', status: 'close', rating: 3.5, image: 'assets/images/shah.jpg' };
+    const Luckybakery = { id: 104, name: 'Lucky Bakery', status: 'open', rating: 3.0, image: 'assets/images/luc.jpg' };
 
     this.trendingList = [karbakery, Paradisebakery, shahgousebakery, Luckybakery];
 
