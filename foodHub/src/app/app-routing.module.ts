7ccb591768073acf11ec6b4433ef5e06c88a3ed1
nallabolu <kalyan.ns@gmail.com>
2018-08-24 10:17:30 +0530

    Day-05
    Day-05 learnings..

--- foodHub/src/app/app-routing.module.ts
+++ foodHub/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ResturantComponent } from './resturant/resturant.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [{ path: '', redirectTo: 'login', pathMatch: 'full' },
 { path: 'login', component: LoginComponent },
 { path: 'Resturant', component: ResturantComponent },
 { path: 'OrderHistory', component: OrderHistoryComponent },
+{ path: 'Details', component: DetailsComponent },
 { path: '**', redirectTo: 'Resturant' }]
 
 @NgModule({
